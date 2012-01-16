2b0057910ff28e6b3bda85989c12496e2514c2e4
BalducciThibaut@gmail.com <BalducciThibaut@gmail.com@a99d3b02-6adb-135f-f965-6542ab8d5abb>
2012-01-16 21:37:02 +0000

    yoo

--- Robot2/others/plusoumoin.py
+++ Robot2/others/plusoumoin.py
@@ -10,18 +10,21 @@ from random import randint
 
 
 def alea (min , max) :
-    return randint(min, max)
-min, max = 1, 100
+
+    return randint(min=1, max=100)
+
 n_entre, n_mystere = 0, alea(min, max) 
-
 def verif(n_entre, n_mystere) :
-  if n_mystere < n_entre :
-    print ("Moin")
-  elif n_mystere > n_entre :
-    print ("Plus")
-  else :
-    print ("Nemo a trouver !\n")
         
-
+    if n_mystere < n_entre :
+        print ("Moin")
+        
+            elif n_mystere > n_entre :
+            print ("Plus")
+            else :
+        print ("Nemo a trouver !\n")
+    
+while n_mystere = n_entre
+else print (" !\n ")
  
     
