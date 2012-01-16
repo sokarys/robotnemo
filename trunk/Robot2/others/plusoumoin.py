8d01d3b6f3482746f02b2ffed63aa9343e8676ba
BalducciThibaut@gmail.com <BalducciThibaut@gmail.com@a99d3b02-6adb-135f-f965-6542ab8d5abb>
2012-01-16 20:54:45 +0000

    Newss

--- trunk/Robot2/others/plusoumoin.py
+++ trunk/Robot2/others/plusoumoin.py
@@ -4,12 +4,24 @@ Created on 16 janv. 2012
 @author: boulange
 '''
 
-def main():
-    print "hello"
-    print 1+2
-    
+  
+from random import randint
 
-if __name__ == '__main__':
-    main()
-    
+
+
+def alea (min , max) :
+    return randint(min, max)
+min, max = 1, 100
+n_entre, n_mystere = 0, alea(min, max) 
+
+def verif(n_entre, n_mystere) :
+  if n_mystere < n_entre :
+    print ("Moin")
+  elif n_mystere > n_entre :
+    print ("Plus")
+  else :
+    print ("Nemo a trouver !\n")
+        
+
+ 
     
