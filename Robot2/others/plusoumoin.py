2ffb72325c11ec489283955ce3ae10ed749acfe4
olivier.locci@gmail.com <olivier.locci@gmail.com@a99d3b02-6adb-135f-f965-6542ab8d5abb>
2012-01-24 19:14:39 +0000

    Fixed some bug in plusoumoin.py.

--- Robot2/others/plusoumoin.py
+++ Robot2/others/plusoumoin.py
@@ -14,17 +14,17 @@ def alea (min , max) :
     return randint(min=1, max=100)
 
 n_entre, n_mystere = 0, alea(min, max) 
+
 def verif(n_entre, n_mystere) :
         
     if n_mystere < n_entre :
-        print ("Moin")
-        
-            elif n_mystere > n_entre :
-            print ("Plus")
-            else :
+        print ("Moin")    
+    elif n_mystere > n_entre :
+        print ("Plus")
+    else :
         print ("Nemo a trouver !\n")
     
-while n_mystere = n_entre
-else print (" !\n ")
+while n_mystere == n_entre:
+    print (" !\n ")
  
     
