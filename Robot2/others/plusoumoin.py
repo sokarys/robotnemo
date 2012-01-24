b0e405aa43fec847fdc7bb193164a4fdf5aadb6a
olivier.locci@gmail.com <olivier.locci@gmail.com@a99d3b02-6adb-135f-f965-6542ab8d5abb>
2012-01-24 19:35:51 +0000

    plusoumoin.py works.

--- Robot2/others/plusoumoin.py
+++ Robot2/others/plusoumoin.py
@@ -9,22 +9,52 @@ from random import randint
 
 
 
-def alea (min=1 , max=100) :
-    
-    return randint(min, max)
-
-n_entre, n_mystere = 0, alea(min, max) 
+n_entre, n_mystere = 0, randint(1, 100) 
 
 def verif(n_entre, n_mystere) :
-        
     if n_mystere < n_entre :
-        print ("Moin")    
+        print ("Moin") 
     elif n_mystere > n_entre :
         print ("Plus")
     else :
         print ("Nemo a trouver !\n")
+        return True
     
-while n_mystere == n_entre:
-    print (" !\n ")
+    return False
+
+def entree_nombre():
+    return int(raw_input("Entrez un nb\n"))
+
+while True:
+    n_entre = entree_nombre()
+    if verif(n_entre, n_mystere):
+        break
  
-    
+ 
+
+#import math
+#import random
+#import os
+#class Game(object):
+#    def __init__(self):
+#        self.__randNumber = int(random.random()*100)
+#        self.__NbPlayer = self.__randNumber + 1
+#        print "Number:", self.__randNumber
+#        
+#    def run(self):
+#        while self.__NbPlayer != self.__randNumber:
+#            try:
+#                self.__NbPlayer = int(raw_input("Entrez un nb\n"))
+#            except:
+#                exit("Erreur, merci de rentrer un nombre fin du programme")
+#            print self.__NbPlayer , self.__randNumber
+#            if self.__NbPlayer < self.__randNumber :
+#                print "+"
+#            elif self.__NbPlayer > self.__randNumber :
+#                print "-"
+#            else:
+#                print "win"
+#        
+##Normalement il y a un if pour verif si on est dans le programme principale
+#game = Game()
+#game.run()
